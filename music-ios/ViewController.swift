6c9a7ed8f95d122f98092ab8aa66e99ad7887ad1
Valeriy Zhuravlev <valerycrane@yandex.ru>
2024-04-04 14:47:26 +0500

    Initial Commit

---
+++ music-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  music-ios
+//
+//  Created by valeriy.zhuravlev on 04.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
